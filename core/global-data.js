d55a5f87c94a8a2bdc3bc93515753b017ea2b472
Rodrigo Sancho <trofrigo@gmail.com>
2019-01-28 21:24:40 +0100

    [refactor] Migration to Polymer 3: Rewritting  components and tests

--- core/global-data.js
+++ core/global-data.js
@@ -1,113 +1,102 @@
 /**
  * Created by Rodrigo on 25/01/2017.
  */
+const data = {};
+const subscribers = {};
+const EVENTS = {
+    SET: 'set',
+    SUBSCRIBE : 'subscribe',
+    UNSUBSCRIBE: 'unsubscribe'
+};
 
-"use strict";
+/**
+ * Notify to all the subscriber the new value.
+ * A subscriber must implement a _onEvent method.
+ * @param event
+ * @param detail
+ * @param path
+ * @private
+ */
+const _notify = (event, detail, path = '') => {
+    // This method will notify to elements subscribed to the path,
+    // as well as the element who are subscribed to every single action, as the global-data element.
+    const pathSubscribers = subscribers[path] || [];
+    const wildCardSubscribers = subscribers['*'] || [];
 
-var PolymerGlobalData = (function () {
+    [...pathSubscribers, ...wildCardSubscribers].forEach(subscriber => {
+        subscriber._onEvent(event, detail);
+    });
+};
 
-    var instance,
-        data,
-        subscribers;
+const _addSubscriber = (path, subscriber) => {
+    subscribers[path] = subscribers[path] || [];
+    subscribers[path].push(subscriber);
+};
 
-    /**
-     * Notify to all the subscriber the new value.
-     * A subscriber must implement a _onEvent method.
-     * @param event
-     * @param detail
-     * @param path
-     * @private
-     */
-    var _notify = function (event, detail, path) {
-        // This method will notify to elements subscribed to the path,
-        // as well as the element who are subscribed to every single action, as the global-data element.
-        var subscribersToNotify = [];
+const _deleteSubscriber = (path, subscriber) => {
+    const index = subscribers[path].indexOf(subscriber);
+    if (index >= 0) subscribers[path].splice(index, 1);
+    if (subscribers[path].length === 0) delete subscribers[path];
+};
 
-        if(typeof subscribers['*'] != 'undefined')
-            subscribersToNotify = subscribersToNotify.concat(subscribers['*']);
+const get = path => data[path];
 
-        if(typeof path != 'undefined' && typeof subscribers[path] != 'undefined')
-            subscribersToNotify = subscribersToNotify.concat(subscribers[path]);
+const set = (path, value) => {
+    if (data[path] === value) {
+        return
+    }
 
-        subscribersToNotify.forEach(function (subscriber) {
-            subscriber._onEvent(event, detail);
-        });
-    };
+    const { SET } = EVENTS;
+    data[path] = value;
+    _notify(SET, {
+        path,
+        value
+    }, path);
+};
 
-    var _init = function (initData) {
+const subscribe = (path, subscriber) => {
+    const { SUBSCRIBE } = EVENTS;
+    _addSubscriber(path, subscriber)
+    _notify(SUBSCRIBE, {
+        path,
+        element: subscriber
+    })
+};
 
-        data = initData || {}; //TODO Must be an object
-        subscribers = {};
+const unsubscribe = (path, subscriber) => {
+    if (!subscribers[path]) {
+        return
+    }
 
-        //Public API
-        return {
-            get: function (path) {
-                return data[path];
-            },
+    const { UNSUBSCRIBE } = EVENTS;
+    _deleteSubscriber(path, subscriber);
+    _notify(UNSUBSCRIBE, {
+        path,
+        element: subscriber
+    });
+};
 
-            set: function (path, value) {
-                if (data[path] != value) {
-                    data[path] = value;
-                    _notify('set', {
-                        path: path,
-                        value: value
-                    }, path);
-                }
-            },
+/**
+ * If the element is subscribed, will return the path of the subscription. If not, will return `false`
+ * @param subscriber
+ * @returns {String | false}
+ */
+const isSubcribed = subscriber =>
+    Object
+        .entries(subscribers)
+        .reduce((acc, [path, subs]) =>
+            subs.contains(subscriber) ? path : acc
+        , false)
 
-            subscribe: function (path, subscriber) {
-                subscribers[path] = subscribers[path] || [];
-                subscribers[path].push(subscriber);
-                _notify('subscribe', {
-                    path: path,
-                    element: subscriber
-                })
-            },
+export default {
+    EVENTS,
+    get,
+    set,
+    subscribe,
+    unsubscribe,
+    isSubcribed
+};
 
-            unsubscribe: function (path, subscriber) {
-                if (typeof subscribers[path] != 'undefined') {
-                    var index = subscribers[path].indexOf(subscriber);
-                    if (index >= 0) subscribers[path].splice(index, 1);
-                    if (subscribers[path].length === 0) delete subscribers[path];
-                    _notify('unsubscribe', {
-                        path: path,
-                        element: subscriber
-                    });
-                }
-            },
 
-            /**
-             * If the element is subscribed, will return the path of the subscription. If not, will return `false`
-             * @param subscriber
-             * @returns {String | false}
-             */
-            isSubcribed: function (subscriber) {
-                for (var prop in subscribers) {
-                    if (subscribers.hasOwnProperty(prop)
-                        && subscribers[prop].indexOf(subscriber) >= 0)
-                        return prop;
-                }
-                return false;
-            }
-        }
-    };
-
-    return {
-        /**
-         * Initiates the only object that can be loaded.
-         * This singleton pattern allow to share data between all the instances
-         * @param data {Object} Initial data
-         * @returns {*}
-         */
-        init: function (data) {
-            if (!instance) {
-                instance = _init(data)
-            }
-
-            return instance;
-        }
-    };
-
-})();
 
 
